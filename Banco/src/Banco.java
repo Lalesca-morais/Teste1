6b4d1d0a37ed903808c4e88e056576b9328307fb
Ana.Vitoria.Nunes <ana.nunes@zup.com.br>
2023-06-23 10:51:22 -0300

    Criação de novo metodo para depositar.

--- Banco/src/Banco.java
+++ Banco/src/Banco.java
@@ -8,9 +8,17 @@ public class Banco {
     public void sacar(double valor) {
         if (valor > 0 && valor <= saldo) {
             saldo -= valor;
-            System.out.println("Saque de R$" + valor + " realizado com sucesso.");
+            System.out.println("Saque de R$" + valor + " realizado com sucesso.\nSaldo Atual: "+saldo);
         } else {
             System.out.println("Saldo insuficiente para realizar o saque.");
+        }
+    }
+    public void depositar(double valor) {
+        if (valor > 0) {
+            saldo += valor;
+            System.out.println("Depósito de R$" + valor + " realizado com sucesso.\nSaldo Atual:"+saldo);
+        } else {
+            System.out.println("Valor inválido para depósito.");
         }
     }
 }
--- Banco/src/Main.java
+++ Banco/src/Main.java
@@ -2,6 +2,10 @@ public class Main {
     public static void main(String[] args) {
         Banco banco = new Banco("José", 1000.0);
         banco.sacar(500.0);
+        banco.depositar(950);
+        banco.sacar(500.0);
+        banco.sacar(500.0);
+        banco.sacar(500.0);
 
     }
 }
